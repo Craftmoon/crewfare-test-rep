5c1a1000ffe5feea5a90af481b929a470ec22eb4
Craftmoon <v.augustosilva@gmail.com>
2025-02-27 23:59:40 -0300

    feat: add domain files for consts and interfaces

---
+++ src/components/domain/constants.ts
@@ -0,0 +1,10 @@
+export enum EventType {
+  PUBLIC = "Public Event",
+  PRIVATE = "Private Event",
+  OTHER = "Other Event",
+}
+
+export enum TaxFeeTypes {
+  FIXED = "Fixed",
+  PERCENTAGE = "Percentage",
+}
---
+++ src/components/domain/interfaces.ts
@@ -0,0 +1,48 @@
+import { EventType, TaxFeeTypes } from "./constants";
+
+export interface Step {
+  title: string;
+  isCompleted: boolean;
+  hasError: boolean;
+}
+
+export interface FormFooterProps {
+  onSave: () => void;
+  onBack?: () => void;
+  onForward?: () => void;
+}
+
+export interface StepItem {
+  title: string;
+  isCompleted: boolean;
+  hasError: boolean;
+}
+
+export interface TaxFee {
+  name: string;
+  amount: number;
+  type: TaxFeeTypes;
+}
+
+export interface EventDate {
+  startDate: Date;
+  endDate: Date;
+}
+
+export interface FormData {
+  eventType: EventType;
+  eventName: string;
+  enabled: boolean;
+  overlayTitleOnBanner: boolean;
+  overlayTitle: string;
+  bannerImage?: File;
+  subdomain?: string;
+  eventAddress?: string;
+  venueName?: string;
+  featuredHotelsTitle?: string;
+  minimumNights?: number;
+  taxesAndFees: TaxFee[];
+  bookableDates: [Date, Date];
+  checkInAndOut: [Date, Date];
+  eventDates: EventDate[];
+}
